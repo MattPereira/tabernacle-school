841961af79ae2d8a7fdcde32c165ab0888890151
Matthew Pereira <matthewdavidpereira@gmail.com>
2023-06-28 15:55:15 -0700

    reorganize folder structure

---
+++ frontend/src/secrets.js
@@ -0,0 +1,8 @@
+//GOOGLE API KEY : AIzaSyBvYyqG8cjiq7D0oBz5akpG96qi40iWJfA
+
+const googleApiKey = "AIzaSyBvYyqG8cjiq7D0oBz5akpG96qi40iWJfA";
+
+const contactServiceId = "service_8c5vd8g";
+const scheduleServiceId = "service_7qwuduh";
+
+module.exports = { googleApiKey, contactServiceId, scheduleServiceId };
