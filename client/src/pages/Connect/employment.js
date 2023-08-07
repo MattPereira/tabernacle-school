59e3843868db2e4afcd027b5e574046bbf79a5fe
Matthew Pereira <matthewdavidpereira@gmail.com>
2023-08-07 07:55:20 -0700

    update employment section

--- client/src/pages/Connect/employment.js
+++ client/src/pages/Connect/employment.js
@@ -1,65 +1,55 @@
 import { Container, Box, Grid, Typography, Button } from "@mui/material";
 import SectionTitle from "../../components/SectionTitle";
-import employmentImg from "../../assets/images/showcase/admissions.jpg";
 
 export default function Employment() {
   return (
     <Box sx={{ py: 8, bgcolor: "background.alternate" }}>
       <SectionTitle title="Employment" />
       <Container>
+        <Box sx={{ mb: 5 }}>
+          <Typography variant="p">
+            We are hiring! Tabernacle School is hiring substitute teachers and
+            support staff for the 2023-2024 school year. All elementary and
+            middle school teaching staff must hold a bachelor's degree from an
+            accredited university and be able to sign the school Statement of
+            Faith. Tabernacle School is an evangelical Christian school focused
+            on providing an excellent Christ centered education in spiritual,
+            academic and extracurricular subjects. Our teachers and staff help
+            students acquire the skills and attitudes necessary to be Faithful
+            Followers of Jesus Christ, Life-Long Learners, Citizens of Character
+            and Academic Achievers.{" "}
+          </Typography>
+        </Box>
+        <Box sx={{ textAlign: "center", mb: 5 }}>
+          <Button
+            component="a"
+            href="https://drive.google.com/file/d/155waZK-5Jq3IwcDit2wTdlNtBAxpqeKW/view?usp=sharing"
+            target="_blank"
+            rel="noopener noreferrer"
+            variant="contained"
+            sx={{
+              fontFamily: "Didact Gothic",
+              fontWeight: "bold",
+              fontSize: "1.2rem",
+            }}
+          >
+            Employment Application
+          </Button>
+        </Box>
         <Grid container spacing={4}>
           <Grid item lg={6}>
-            <Box
-              component="img"
-              src={employmentImg}
-              alt="join our team"
-              sx={{ width: "100%", borderRadius: "30px", mb: 3 }}
-            />
-
             <Box sx={{ mb: 3 }}>
+              <Typography variant="h5">Open Positions</Typography>
               <Typography variant="p">
-                We are hiring! Tabernacle School is hiring substitute teachers
-                and support staff for the 2022-2023 school year. All elementary
-                and middle school teaching staff must hold a bachelor's degree
-                from an accredited university and be able to sign the school
-                Statement of Faith. Tabernacle School is an evangelical
-                Christian school focused on providing an excellent Christ
-                centered education in spiritual, academic and extracurricular
-                subjects. Our teachers and staff help students acquire the
-                skills and attitudes necessary to be Faithful Followers of Jesus
-                Christ, Life-Long Learners, Citizens of Character and Academic
-                Achievers.{" "}
-              </Typography>
-            </Box>
-            <Box sx={{ textAlign: "center" }}>
-              <Button
-                component="a"
-                href="https://drive.google.com/file/d/155waZK-5Jq3IwcDit2wTdlNtBAxpqeKW/view?usp=sharing"
-                target="_blank"
-                rel="noopener noreferrer"
-                variant="contained"
-                sx={{
-                  fontFamily: "Didact Gothic",
-                  fontWeight: "bold",
-                  fontSize: "1.2rem",
-                }}
-              >
-                Employment Application
-              </Button>
-            </Box>
-          </Grid>
-          <Grid item lg={6}>
-            <Box sx={{ mb: 3 }}>
-              <Typography variant="h5">
-                Now Hiring Preschool Teachers!
-              </Typography>
-              <Typography variant="p">
-                Tabernacle Preschool is looking for part time candidates who
-                have at least six ECE units. Please contact Betty Mason at{" "}
-                <a href="mailto:ElizabethM@tbs.org">ElizabethM@tbs.org</a> or{" "}
+                Tabernacle School is looking for a part time middle school
+                science teacher for the current 2023-2024 school year. All
+                applicants must hold a bachelor's degree from an accredited
+                university and be able to sign the school Statement of Faith.
+                Please contact Lesley{" "}
+                <a href="mailto:LesleyN@tbs.org">LesleyN@tbs.org</a> or call
                 <a href="tel:925-685-9169">925-685-9169</a> for more
-                information. Applications may also be picked up in school
-                office.
+                information. Applications may also be picked up in the school's
+                front office.
               </Typography>
             </Box>
 
@@ -72,6 +62,8 @@ export default function Employment() {
                 <li>Three letters of reference</li>
               </ol>
             </Typography>
+          </Grid>
+          <Grid item lg={6}>
             <Box sx={{ mb: 3 }}>
               <Typography variant="h5">Spiritual Requirements</Typography>
               <Typography variant="p">
