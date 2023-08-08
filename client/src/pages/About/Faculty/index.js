2c4da7e7ad2fda0d65ec44c3c611136d45390478
Matthew Pereira <matthewdavidpereira@gmail.com>
2023-08-07 21:44:19 -0700

    bigger buttons

--- client/src/pages/About/Faculty/index.js
+++ client/src/pages/About/Faculty/index.js
@@ -2,136 +2,37 @@
 import SectionTitle from "../../../components/SectionTitle";
 import { Link } from "react-router-dom";
 // prettier-ignore
-import { Container, Grid, Box, FormControl, Select, MenuItem, Typography, Button } from "@mui/material";
+import { Container, Grid, Box, Typography, Button } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
 import LoadingSpinner from "../../../components/LoadingSpinner";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
 // faculty slideshow
-import Carousel from "react-material-ui-carousel";
-import slide1 from "../../../assets/images/about/slides/slide1.jpg";
-import slide2 from "../../../assets/images/about/slides/slide2.jpg";
-import slide3 from "../../../assets/images/about/slides/slide3.jpg";
-import slide4 from "../../../assets/images/about/slides/slide4.jpg";
-import slide5 from "../../../assets/images/about/slides/slide5.jpg";
-import slide6 from "../../../assets/images/about/slides/slide6.jpg";
+// import Carousel from "react-material-ui-carousel";
+// import slide1 from "../../../assets/images/about/slides/slide1.jpg";
+// import slide2 from "../../../assets/images/about/slides/slide2.jpg";
+// import slide3 from "../../../assets/images/about/slides/slide3.jpg";
+// import slide4 from "../../../assets/images/about/slides/slide4.jpg";
+// import slide5 from "../../../assets/images/about/slides/slide5.jpg";
+// import slide6 from "../../../assets/images/about/slides/slide6.jpg";
 
 export default function Faculty() {
   return (
     <Box sx={{ py: 8, bgcolor: "background.alternate" }}>
       <SectionTitle title="Faculty" />
       <Container>
-        <Grid container sx={{ justifyContent: "center" }}>
-          <Grid item xl={10}>
-            <Slides />
-            <SelectGrade />
-          </Grid>
-        </Grid>
+        {/* <Slides /> */}
+        <SelectGrade />
       </Container>
     </Box>
   );
 }
-const images = [slide1, slide2, slide3, slide4, slide5, slide6];
-
-const Slides = () => {
-  const [imagesLoaded, setImagesLoaded] = useState(false);
-
-  useEffect(() => {
-    const preLoadImages = async () => {
-      const promises = images.map((src) => {
-        return new Promise((resolve, reject) => {
-          const img = new Image();
-          img.src = src;
-          img.onload = resolve;
-          img.onerror = reject;
-        });
-      });
-
-      await Promise.all(promises);
-      setImagesLoaded(true);
-    };
-
-    preLoadImages();
-  }, []);
-
-  if (!imagesLoaded) {
-    return null; // or return a placeholder/spinner
-  }
-
-  return (
-    <Box sx={{ mb: 5 }}>
-      <Carousel indicators={false} interval={7000} duration={1500}>
-        <Grid container>
-          <Grid item md={6}>
-            <Box
-              component="img"
-              sx={{ width: "100%" }}
-              src={slide1}
-              alt="First slide"
-            />
-          </Grid>
-          <Grid item md={6} sx={{ display: { xs: "none", md: "flex" } }}>
-            <Box
-              component="img"
-              sx={{ width: "100%" }}
-              src={slide6}
-              alt="First slide"
-            />
-          </Grid>
-        </Grid>
-
-        <Grid container>
-          <Grid item md={6}>
-            <Box
-              component="img"
-              sx={{ width: "100%" }}
-              src={slide2}
-              alt="First slide"
-            />
-          </Grid>
-          <Grid item md={6} sx={{ display: { xs: "none", md: "flex" } }}>
-            <Box
-              component="img"
-              sx={{ width: "100%" }}
-              src={slide3}
-              alt="First slide"
-            />
-          </Grid>
-        </Grid>
-
-        <Grid container>
-          <Grid item md={6}>
-            <Box
-              component="img"
-              sx={{ width: "100%" }}
-              src={slide4}
-              alt="First slide"
-            />
-          </Grid>
-          <Grid item md={6} sx={{ display: { xs: "none", md: "flex" } }}>
-            <Box
-              component="img"
-              sx={{ width: "100%" }}
-              src={slide5}
-              alt="First slide"
-            />
-          </Grid>
-        </Grid>
-      </Carousel>
-    </Box>
-  );
-};
 
 function SelectGrade() {
   const [facultyData, setFacultyData] = useState(null);
   const [selection, setSelection] = useState(null);
   const theme = useTheme();
-  console.log("selection", selection);
-
-  const handleChange = (event) => {
-    setSelection(event.target.value);
-  };
 
   useEffect(function fetchFacultyData() {
     async function fetchData() {
@@ -169,132 +70,141 @@ function SelectGrade() {
           page.
         </Typography>
       </Box>
+      <Box
+        sx={{
+          minHeight: "600px",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+        }}
+      >
+        {selection ? (
+          <Box>
+            <Typography
+              variant="h4"
+              sx={{ textDecoration: "underline" }}
+              textAlign="center"
+            >
+              {facultyObj[selection].name}
+            </Typography>
+            <Grid container spacing={4} sx={{ py: 5 }}>
+              {facultyObj[selection].staff_members.data.map((member) => {
+                const {
+                  email,
+                  name,
+                  profile_picture,
+                  title_short: titleShort,
+                } = member.attributes;
 
-      {selection ? (
-        <>
-          <Typography
-            variant="h4"
-            sx={{ textDecoration: "underline" }}
-            textAlign="center"
-          >
-            {facultyObj[selection].name}
-          </Typography>
-          <Grid container spacing={4} sx={{ py: 5 }}>
-            {facultyObj[selection].staff_members.data.map((member) => {
-              const {
-                email,
-                name,
-                profile_picture,
-                title_short: titleShort,
-              } = member.attributes;
+                const profilePicture = profile_picture.data.attributes.url;
+                return (
+                  <Grid
+                    item
+                    key={member.id}
+                    xs={6}
+                    sm={6}
+                    lg={3}
+                    sx={{ textAlign: "center" }}
+                  >
+                    <Link
+                      to={`/about/staff/${email}`}
+                      style={{
+                        textDecoration: "none",
+                        color: theme.palette.text.primary,
+                      }}
+                    >
+                      <Box sx={{ mb: 1 }}>
+                        <Typography variant="p" sx={{ whiteSpace: "nowrap" }}>
+                          {name}
+                        </Typography>
+                      </Box>
+                      <Box
+                        sx={{
+                          width: 150,
+                          height: 150,
+                          borderRadius: "50%",
+                          overflow: "hidden",
+                          mx: "auto",
+                        }}
+                      >
+                        <Box
+                          component="img"
+                          sx={{
+                            width: "100%",
+                            height: "100%",
+                            objectFit: "cover",
+                            objectPosition: "center",
+                          }}
+                          src={profilePicture}
+                        />
+                      </Box>
+                      <Typography variant="p">{titleShort}</Typography>
+                    </Link>
+                  </Grid>
+                );
+              })}
+            </Grid>
+            <Box sx={{ textAlign: "end" }}>
+              <Typography
+                variant="p"
+                sx={{
+                  textDecoration: "underline",
+                  cursor: "pointer",
+                  color: theme.palette.primary.main,
+                }}
+                onClick={() => setSelection(null)}
+              >
+                See all faculty
+              </Typography>
+            </Box>
+          </Box>
+        ) : (
+          <Grid container spacing="20">
+            {facultyOptions.map((option) => {
+              let shortName;
 
-              const profilePicture = profile_picture.data.attributes.url;
+              if (option === "Middle School") {
+                shortName = "Jr High";
+              } else if (option === "Physical Education") {
+                shortName = "Phys Ed";
+              } else {
+                shortName = option;
+              }
+
               return (
                 <Grid
                   item
-                  key={member.id}
                   xs={6}
-                  sm={6}
-                  lg={3}
-                  sx={{ textAlign: "center" }}
+                  lg={4}
+                  key={option}
+                  sx={{ display: "flex", justifyContent: "center" }}
                 >
-                  <Link
-                    to={`/about/staff/${email}`}
-                    style={{
-                      textDecoration: "none",
-                      color: theme.palette.text.primary,
+                  <Button
+                    variant="outlined"
+                    onClick={() => setSelection(option)}
+                    sx={{
+                      borderRadius: "15px",
+                      width: "100%",
+                      color: "black",
+                      borderColor: "black",
+                      fontFamily: "didact gothic",
+                      fontWeight: "normal",
+                      py: 3,
+                      "&:hover": {
+                        bgcolor: "black",
+                        color: "white",
+                        borderColor: "black",
+                      },
                     }}
                   >
-                    <Box sx={{ mb: 1 }}>
-                      <Typography variant="p" sx={{ whiteSpace: "nowrap" }}>
-                        {name}
-                      </Typography>
-                    </Box>
-                    <Box
-                      sx={{
-                        width: 150,
-                        height: 150,
-                        borderRadius: "50%",
-                        overflow: "hidden",
-                        mx: "auto",
-                      }}
-                    >
-                      <Box
-                        component="img"
-                        sx={{
-                          width: "100%",
-                          height: "100%",
-                          objectFit: "cover",
-                          objectPosition: "center",
-                        }}
-                        src={profilePicture}
-                      />
-                    </Box>
-                    <Typography variant="p">{titleShort}</Typography>
-                  </Link>
+                    {shortName}
+                  </Button>
                 </Grid>
               );
             })}
           </Grid>
-          <Box sx={{ textAlign: "end" }}>
-            <Typography
-              variant="p"
-              sx={{
-                textDecoration: "underline",
-                cursor: "pointer",
-                color: theme.palette.primary.main,
-              }}
-              onClick={() => setSelection(null)}
-            >
-              See all faculty
-            </Typography>
-          </Box>
-        </>
-      ) : (
-        <Grid container spacing="20">
-          {facultyOptions.map((option) => {
-            let shortName;
-
-            if (option === "Middle School") {
-              shortName = "Jr High";
-            } else if (option === "Physical Education") {
-              shortName = "Phys Ed";
-            } else {
-              shortName = option;
-            }
-
-            return (
-              <Grid
-                item
-                xs={6}
-                lg={4}
-                key={option}
-                sx={{ display: "flex", justifyContent: "center" }}
-              >
-                <Button
-                  variant="outlined"
-                  onClick={() => setSelection(option)}
-                  sx={{
-                    borderRadius: "15px",
-                    width: "100%",
-                    color: "black",
-                    borderColor: "black",
-                    fontFamily: "didact gothic",
-                    fontWeight: "normal",
-                    "&:hover": {
-                      bgcolor: "black",
-                      color: "white",
-                      borderColor: "black",
-                    },
-                  }}
-                >
-                  {shortName}
-                </Button>
-              </Grid>
-            );
-          })}
-        </Grid>
-      )}
+        )}
+      </Box>
     </Box>
   );
 }
