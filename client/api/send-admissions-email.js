4939c573005ed9bb58be56be89e5f73586c04af0
Matthew Pereira <matthewdavidpereira@gmail.com>
2023-09-13 13:44:18 -0700

    uncomment send email logic

--- client/api/send-admissions-email.js
+++ client/api/send-admissions-email.js
@@ -39,27 +39,27 @@ export default async function handler(request, response) {
     }
 
     try {
-      // await client.sendEmail({
-      //   From: process.env.EMAIL_ADDRESS,
-      //   to: process.env.EMAIL_ADDRESS,
-      //   Subject: "Schedule Tour Request from tabernacle.school",
-      //   HtmlBody: `
-      //   <h3>Name </h3>
-      //   <p>${name}</p>
-      //   <h3>Email </h3>
-      //   <p>${email}</p>
-      //   <h3>Subject</h3>
-      //   <p>Scheduling a tour of tabernacle campus</p>
-      //   <h3>Message</h3>
-      //   <p>${message}</p>
-      //   `,
-      // });
+      await client.sendEmail({
+        From: process.env.EMAIL_ADDRESS,
+        to: process.env.EMAIL_ADDRESS,
+        Subject: "Schedule Tour Request from tabernacle.school",
+        HtmlBody: `
+        <h3>Name </h3>
+        <p>${name}</p>
+        <h3>Email </h3>
+        <p>${email}</p>
+        <h3>Subject</h3>
+        <p>Scheduling a tour of tabernacle campus</p>
+        <h3>Message</h3>
+        <p>${message}</p>
+        `,
+      });
 
       return response.status(200).json({ status: "Email sent successfully!" });
     } catch (error) {
       return response
         .status(500)
-        .json({ message: "Server error email not sent" });
+        .json({ error: "Server error email not sent" });
     }
   } else {
     return response.status(405).json({ message: "Method Not Allowed!!!" });
