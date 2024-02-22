597ab8b2070f0511220b6394e74d24f4fa22672e
MattPereira <mattpereira.dev@gmail.com>
2024-02-22 14:29:14 -0800

    add buildathon links

--- nextjs/components/home/SchoolSpotlight.jsx
+++ nextjs/components/home/SchoolSpotlight.jsx
@@ -28,12 +28,24 @@ export const SchoolSpotlight = () => {
         </div>
         <div>
           <h5 className="text-2xl font-bold text-center mb-5">
-            Upcoming Events
+            Buildathon
           </h5>
-          <p className="text-xl mb-5">
-            Get ready for this year's annual Jog-a-thon! More information coming
-            soon.
-          </p>
+          <div className="mb-3">
+            <a target="_blank"
+              rel="noopener noreferrer" className="text-xl text-primary underline" href="https://pledgestar.com/tabernacle/">PledgeStar</a>
+
+          </div>
+          <div className="mb-3">
+            <a target="_blank"
+              rel="noopener noreferrer" className="text-xl text-primary underline" href="https://www.signupgenius.com/go/10C094BAAAB2AA1F4C52-2023#/">Volunteer Signup</a>
+
+          </div>
+          <div className="mb-3">
+            <a target="_blank"
+              rel="noopener noreferrer" className="text-xl text-primary underline" href="https://www.signupgenius.com/go/10C094BAAAB2AA1F4C52-47302779-construction">Parent Lunch Order</a>
+          </div>
+
+
         </div>
       </div>
     </SectionWrapper>
